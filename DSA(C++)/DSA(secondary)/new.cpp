d06fc22584ea8ef799b2603abd6c0db807bc1f8d
Tuhin874 <tuhin.sarkar874@gmail.com>
2024-08-29 19:14:15 +0530

    Learning c++ Day-2

--- DSA(C++)/DSA(secondary)/new.cpp
+++ DSA(C++)/DSA(secondary)/new.cpp
@@ -252,3 +252,103 @@
     
     
 // }
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+
+// int main () {
+//     int n;
+//     cin>>n;
+
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n-i; j++)
+//         {
+//             cout<<" ";
+//         }
+//         for (int j = 1; j <= n; j++)
+//         {
+//             cout<<"*";
+//         }
+//         cout<<endl;
+        
+        
+//     }
+    
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+
+// int main (){
+//     int n;
+//     cin>>n;
+
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n-i; j++)
+//         {
+//             cout<<" ";
+//         }
+//         for (int j = 1; j <=i; j++)
+//         {
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+        
+//     }
+    
+// }
+
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+
+int main (){
+    int n;
+    cin>>n;
+
+
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        int j;
+
+        for (j = 1; j <=n-i; j++)
+        {
+            cout<<"  ";
+        }
+
+        int k =  i;
+        
+        for (; j <=n; j++)
+        {
+            cout<<k--<<" ";
+        }
+        k = 2;
+        for (; j <= n+i-1; j++)
+        {
+            cout<<k++<<" ";
+        }
+        cout<<endl;
+        
+    }
+    
+    
+}
